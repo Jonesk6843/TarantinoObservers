3b28ce12f7a3a1aca1a2a15611b6dae92cc2ada8
Parker Pawn <Parker Pawn@KENT-JONES-JR>
2022-02-11 00:27:24 -0500

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,21 @@
+﻿/**
+* 2/10/22
+* CSC 153
+* Kent Jones Jr
+* A text adventure of the first level of the Tarantino Observers.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
