1e91cf5f96da1ac3f9142fa8ef525c534d860c07
Kent Jones Jr <jonesk6843@student.faytechcc.edu>
2022-02-12 00:13:52 -0500

    Added sorting tags for arrays and lists.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -16,21 +16,29 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            //variables
+            int steps = 0;
+
             //Arrays
             string[] rooms = new string[5] {"Entrance", "Abandoned Lobby Room", "Hallway", "Hallway #2", "King's Quarters"};
             string[] weapons = new string[4] {"Lance of the Round Table", "Shield of the Round Table", "Broken Sword", "Hammer of the High Orc"};
+            Array.Sort<string>(weapons);
             string[] potions = new string[2] {"Elixer of Healing", "Stone Skin"};
+            Array.Sort<string>(potions);
             string[] treasures = new string[3] {"Gold Pouch", "Goblin eye", "Crown of the High Orc"};
+            Array.Sort<string>(treasures);
 
             //Lists
             List<string> items = new List<string>() {"Observer's Notebook", "Lockpick set", "Ink and feather", "Smoke Bombs"};
+            items.Sort();
             List<string> mobs = new List<string>() {"Spider", "Gold Goblin", "Infantry Goblin", "High Knight Goblin" , "High Orc"};
+            mobs.Sort();
 
             bool exit = false;
             //Main Menu
             while (exit == false)
             {
-                Console.WriteLine("Welcome, Observer!");
+                Console.WriteLine($"Current Location - {rooms[steps]}");
                 Console.WriteLine("1.) Display Rooms");
                 Console.WriteLine("2.) Display Weapons");
                 Console.WriteLine("3.) Display Potions");
@@ -48,36 +56,42 @@ namespace ConsoleUI
                         {
                             Console.WriteLine(val);
                         }
+                        Console.WriteLine();
                         break;
                     case "2":
                         foreach (string val in weapons)
                         {
                             Console.WriteLine(val);
                         }
+                        Console.WriteLine();
                         break;
                     case "3":
                         foreach (string val in potions)
                         {
                             Console.WriteLine(val);
                         }
+                        Console.WriteLine();
                         break;
                     case "4":
                         foreach (string val in treasures)
                         {
                             Console.WriteLine(val);
                         }
+                        Console.WriteLine();
                         break;
                     case "5":
                         foreach (string val in items)
                         {
                             Console.WriteLine(val);
                         }
+                        Console.WriteLine();
                         break;
                     case "6":
                         foreach (string val in mobs)
                         {
                             Console.WriteLine(val);
                         }
+                        Console.WriteLine();
                         break;
                     case "7":
                         exit = true;
@@ -87,77 +101,74 @@ namespace ConsoleUI
                         {
                             Console.WriteLine(val);
                         }
+                        Console.WriteLine();
                         break;
                     case "weapons":
                         foreach (string val in weapons)
                         {
                             Console.WriteLine(val);
                         }
+                        Console.WriteLine();
                         break;
                     case "potions":
                         foreach (string val in potions)
                         {
                             Console.WriteLine(val);
                         }
+                        Console.WriteLine();
                         break;
                     case "treasures":
                         foreach (string val in treasures)
                         {
                             Console.WriteLine(val);
                         }
+                        Console.WriteLine();
                         break;
                     case "items":
                         foreach (string val in items)
                         {
                             Console.WriteLine(val);
                         }
+                        Console.WriteLine();
                         break;
                     case "mobs":
                         foreach (string val in mobs)
                         {
                             Console.WriteLine(val);
                         }
+                        Console.WriteLine();
                         break;
                     case "s":
-                        //TODO
+                        if (steps != 0)
+                        {
+                            steps--;
+                            Console.WriteLine();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Maybe I missed something...");
+                            Console.WriteLine();
+                        }
                         break;
                     case "n":
-                        //TODO
+                        if (steps != 4)
+                        {
+                            steps++;
+                            Console.WriteLine();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Looks like a dead end.");
+                            Console.WriteLine();
+                        }
                         break;
                     default:
-                        //TODO
-                        break;
-
+                        {
+                            Console.WriteLine("Apologies. Come again?");
+                            Console.WriteLine();
+                            break;
+                        }
                 }
-
-                //if (input == "1")
-                //{
-                //    //TODO
-                //}
-                //else if (input == "2")
-                //{
-                    
-                //}
-                //else if (input == "3")
-                //{
-                //    ;
-                //}
-                //else if (input == "4")
-                //{
-                //    ;
-                //}
-                //else if (input == "5")
-                //{
-                //    ;
-                //}
-                //else if (input == "6")
-                //{
-                //    exit = true;
-                //}
-                //else
-                //{
-                //    Console.WriteLine("Apologies. Come again?");
-                //}
             }
         }
     }
