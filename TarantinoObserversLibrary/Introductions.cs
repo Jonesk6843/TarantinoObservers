e4151503d2ea398b7d6a2d074688aab17b7a3643
Kent Jones <jonesk6843@student.faytechcc.edu>
2022-03-16 11:45:38 -0400

    Tasks completed: -Class Library created -CombatMenu and MovementMenu created -MovementMenu completed (No return tho) -Introductions method completed.

---
+++ TarantinoObserversLibrary/Introductions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TarantinoObserversLibrary
+{
+    public class Introductions
+    {
+        public static void WelcomeUser()
+        {
+            //Declaring variables
+            
+            StreamWriter outputFile;
+
+            //Getting player information
+            outputFile = File.CreateText("Observer_Cirtificate.txt");
+            Console.WriteLine("Greetings, Observer!");
+            Console.WriteLine("What is your name? >");
+            string nameInput = Console.ReadLine();
+            outputFile.WriteLine("Observer name:" + nameInput);
+
+            Console.WriteLine(nameInput + ". A name that will ring across the land!");
+            Console.WriteLine("This is your observer handbook. Create a password so that only you may access it's contents. >");
+            string passwordInput = Console.ReadLine();
+            outputFile.WriteLine("Your password is: " + passwordInput);
+            Console.WriteLine("Keep that password safe, okay?");
+            Console.WriteLine("Finally, what class of Observer are you? A (Gunslinger), (Witch), or (standard)? >");
+            string classInput = Console.ReadLine();
+            outputFile.WriteLine("Your Class is:" + classInput);
+
+            Console.WriteLine("Finalized! You are now officially an Observer of the Tarantino Family! Now get out there and observe some monsters!");
+            Console.WriteLine("Remember! Communication amongst monsters is key!");
+            outputFile.Close();
+        }
+    }
+}
---
+++ TarantinoObserversLibrary/MovementMenu.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TarantinoObserversLibrary
+{
+    public class MovementMenu
+    {
+        public static void UserMenu(ref string rooms)
+        {
+            try
+            {
+                //Variables
+                int steps = 0;
+                bool exit = false;
+
+                //main menu
+                while (exit == false)
+                {
+                    Console.WriteLine($"Current Location - {rooms[steps]}");
+                    Console.WriteLine("1.) Move North");
+                    Console.WriteLine("2.) Move South");
+                    Console.WriteLine("3.) Attack");
+                    Console.WriteLine("4.) Exit");
+                    Console.Write("Make your choice, Observer. >");
+                    string input = Console.ReadLine();
+                    switch (input.ToLower())
+                    {
+                        case "1":
+                            if (steps != 0)
+                            {
+                                steps--;
+                                Console.WriteLine();
+                            }
+                            else
+                            {
+                                Console.WriteLine("Maybe I missed something...");
+                                Console.WriteLine();
+                            }
+                            break;
+
+                        case "2":
+                            if (steps != 4)
+                            {
+                                steps++;
+                                Console.WriteLine();
+                            }
+                            else
+                            {
+                                Console.WriteLine("Looks like a dead end.");
+                                Console.WriteLine();
+                            }
+                            break;
+
+                        case "3":
+                            {
+                                Console.WriteLine("Not built yet...");
+                                Console.WriteLine();
+                            }
+                            break;
+
+                        case "4":
+                            exit = true;
+                            break;
+
+                        case "north":
+                            if (steps != 4)
+                            {
+                                steps++;
+                                Console.WriteLine();
+                            }
+                            else
+                            {
+                                Console.WriteLine("Looks like a dead end.");
+                                Console.WriteLine();
+                            }
+                            break;
+
+                        case "South":
+                            if (steps != 0)
+                            {
+                                steps--;
+                                Console.WriteLine();
+                            }
+                            else
+                            {
+                                Console.WriteLine("Maybe I missed something...");
+                                Console.WriteLine();
+                            }
+                            break;
+
+                        case "Combat":
+                            {
+                                Console.WriteLine("Not built yet...");
+                                Console.WriteLine();
+                            }
+                            break;
+
+                        case "exit":
+                            exit = true;
+                            break;
+                        default:
+                            {
+                                Console.WriteLine("Apologies. Come again?");
+                                Console.WriteLine();
+                                break;
+                            }
+                        }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
